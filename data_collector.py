f95350ce1892e8f6f0e786e7cb9955f6b2f1092b
deveshtrain2222-create <deveshinttrain.2222@gmail.com>
2025-12-05 13:38:27 +0530

    Delete data_collector.py

--- data_collector.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yf
-
-
-btc= yf.Ticker("BTC-USD")
-data = btc.history(period="1mo")    
-print(data.head())
-
-data.to_csv("crypto_prices1.csv",index=True)
-print("CSV created successfully: crypto_prices1.csv")
